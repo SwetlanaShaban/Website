a7040a326a6b5f0a0780c872c289602d7f7cc521
Андрей <andrey.naumovich@mail.ru>
2018-05-14 22:49:35 +0300

    fix

--- menuHandlers.js
+++ menuHandlers.js
@@ -1,6 +1,23 @@
-$(document).ready(function(){
-    $(".menuArea").click(function(e){
+$(document).ready(function () {
+    var xhttp = new XMLHttpRequest();
+    var json ;
+
+    xhttp.onreadystatechange = function (data) {
+        if (this.readyState == 4 && this.status == 200) {
+            json =  JSON.parse(xhttp.responseText);
+        }
+    };
+
+    xhttp.open("GET", "signs.json", true);
+    xhttp.send();
+
+    $(".menuArea").click(function (e) {
         var title = $(e.target).attr('title');
-        alert(title);
+        var descriptionObject = getSignDescription(title);
     });
+
+
+    var getSignDescription = function (name) {
+        return json[name];
+    }
 });
